0ce2c1dd27b844dca4f25f3e2a881c2a579c9221
Belland KHAM <bellandkham9@gmail.com>
2026-08-21 20:14:55 +0100

    Configure PostgreSQL production

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -196,7 +196,9 @@ DATABASES = {
                 "DB_PORT",
                 "5433",
             ),
-
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 
 }
@@ -331,38 +333,52 @@ SIMPLE_JWT = {
 # ==========================================================
 # EMAIL
 # ==========================================================
+
 # ==========================================================
 # EMAIL
 # ==========================================================
 
-MAILERS = {
-    "default": {
-        "BACKEND": os.getenv(
-            "EMAIL_BACKEND",
-            "django.core.mail.backends.console.EmailBackend",
-        ),
-        "OPTIONS": {
-            "host": os.getenv("EMAIL_HOST", ""),
-            "port": int(os.getenv("EMAIL_PORT", "587")),
-            "username": os.getenv("EMAIL_HOST_USER", ""),
-            "password": os.getenv("EMAIL_HOST_PASSWORD", ""),
-            "use_tls": os.getenv(
-                "EMAIL_USE_TLS",
-                "True",
-            ).lower() == "true",
-            "use_ssl": os.getenv(
-                "EMAIL_USE_SSL",
-                "False",
-            ).lower() == "true",
-        },
-    },
-}
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.console.EmailBackend",
+)
+
+EMAIL_HOST = os.getenv(
+    "EMAIL_HOST",
+    "",
+)
+
+EMAIL_PORT = int(
+    os.getenv(
+        "EMAIL_PORT",
+        "587",
+    )
+)
+
+EMAIL_HOST_USER = os.getenv(
+    "EMAIL_HOST_USER",
+    "",
+)
+
+EMAIL_HOST_PASSWORD = os.getenv(
+    "EMAIL_HOST_PASSWORD",
+    "",
+)
+
+EMAIL_USE_TLS = os.getenv(
+    "EMAIL_USE_TLS",
+    "True",
+).lower() == "true"
+
+EMAIL_USE_SSL = os.getenv(
+    "EMAIL_USE_SSL",
+    "False",
+).lower() == "true"
 
 DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL",
     "Bahá'í Companion <noreply@example.com>",
 )
-
 # ==========================================================
 # STATIC FILES
 # ==========================================================
